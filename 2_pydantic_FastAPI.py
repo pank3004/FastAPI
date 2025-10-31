9e6f04f0ee7a775696d57bda0fb90b2dbcb44180
pank3004 <pankajkumawatew3004@gmail.com>
2025-10-31 11:33:59 +0530

    basics and pydatic coverd

---
+++ 2_pydantic_FastAPI.py
@@ -0,0 +1,13 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+app=FastAPI()
+
+
+class User(BaseModel): 
+    id: int
+    name:str
+
+@app.get('/user', response_model=User)
+def get_user(): 
+    return User(id=1 , name= 'pankaj')
