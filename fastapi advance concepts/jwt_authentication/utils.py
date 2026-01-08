dcdb30f9f9bd1e9f4a222c3b6965f028be5e18d6
pank3004 <pankajkumarewpk2004@gmail.com>
2026-01-08 18:33:48 +0530

    authentication done

---
+++ fastapi advance concepts/jwt_authentication/utils.py
@@ -0,0 +1,18 @@
+from passlib.context import CryptContext
+
+pwd_context = CryptContext(schemes=['bcrypt'], deprecated='auto')
+
+fake_user_db = {
+    'pankaj': {
+        'username': 'pankaj',
+        'hashed_password': pwd_context.hash('secret123')
+    }
+}
+
+def get_user(username: str):
+    user = fake_user_db.get(username)
+    return user
+
+
+def verify_password(plain_password, hashed_password):
+    return pwd_context.verify(plain_password, hashed_password)
